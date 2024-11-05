e7c209493fe078fa35bf6238060abefe2766e097
Bruce Shih <sp1986@gmail.com>
2024-11-05 17:30:10 +0800

    feat: migrate existed database

--- server/database/migrations/0000_fast_hawkeye.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `images` (
-	`key` text PRIMARY KEY NOT NULL,
-	`nsfw` integer DEFAULT 0 NOT NULL,
-	`favorite` integer DEFAULT 0 NOT NULL,
-	`alive` integer DEFAULT 1 NOT NULL,
-	`createDate` text NOT NULL,
-	`deleteDate` text
-);
