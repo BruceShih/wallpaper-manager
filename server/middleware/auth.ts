c8ad3130670ff09968ff22e80a9ddb5208c3ee6a
Bruce Shih <sp1986@gmail.com>
2024-11-05 22:22:16 +0800

    feat: only protect /api route

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,11 +1,13 @@
 export default defineEventHandler((event) => {
-  const { authKeySecret } = useRuntimeConfig(event)
-  const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
-  if (key !== authKeySecret) {
-    console.error('[Wallpaper Service] Unauthorized')
-    throw createError({
-      statusCode: 401,
-      message: 'Unauthorized'
-    })
+  if (event.path.startsWith('/api')) {
+    const { authKeySecret } = useRuntimeConfig(event)
+    const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
+    if (key !== authKeySecret) {
+      console.error('[Wallpaper Service] Unauthorized')
+      throw createError({
+        statusCode: 401,
+        message: 'Unauthorized'
+      })
+    }
   }
 })
