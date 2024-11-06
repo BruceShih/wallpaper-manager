0e593fe7a824d6002034ac13a408f2a977c15a9e
Bruce Shih <sp1986@gmail.com>
2024-11-06 16:32:41 +0800

    feat: disable email login

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -22,7 +22,7 @@ export function serverAuth() {
       },
       baseURL: getBaseURL(),
       emailAndPassword: {
-        enabled: true
+        enabled: false
       },
       socialProviders: {
         github: {
