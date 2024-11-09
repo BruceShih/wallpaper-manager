4238696ca8418b864d45f93b0d9d3397c73bf520
Bruce Shih <sp1986@gmail.com>
2024-11-09 09:44:02 +0800

    fix: prevent null url

--- server/middleware/apiAuth.ts
+++ server/middleware/apiAuth.ts
@@ -1,26 +1,27 @@
 import process from 'node:process'
 
 export default defineEventHandler((event) => {
-  const url = new URL(event.headers.get('referer') || '')
-
-  // check if the request origin is the same as the API Gateway domain
-  if (url.origin !== process.env.ORIGIN) {
-    const paths = ['/api/delete', '/api/list', '/api/get', '/api/update', '/api/upload']
-    // if event.path starts with any of paths above
-    if (paths.some(path => event.path.startsWith(path))) {
-    // get authKeySecret from runtime config
-      const { authKeySecret } = useRuntimeConfig(event)
-      // get key from request header
-      const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
-      // if key is not equal to authKeySecret
-      if (key !== authKeySecret) {
-      // log unauthorized
-        console.error('[Wallpaper Service] Unauthorized')
-        // throw error
-        throw createError({
-          statusCode: 401,
-          message: 'Unauthorized'
-        })
+  const referer = event.headers.get('referer')
+  if (referer) {
+    // check if the request origin is the same as the API Gateway domain
+    if (new URL(referer).origin !== process.env.ORIGIN) {
+      const paths = ['/api/delete', '/api/list', '/api/get', '/api/update', '/api/upload']
+      // if event.path starts with any of paths above
+      if (paths.some(path => event.path.startsWith(path))) {
+      // get authKeySecret from runtime config
+        const { authKeySecret } = useRuntimeConfig(event)
+        // get key from request header
+        const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
+        // if key is not equal to authKeySecret
+        if (key !== authKeySecret) {
+        // log unauthorized
+          console.error('[Wallpaper Service] Unauthorized')
+          // throw error
+          throw createError({
+            statusCode: 401,
+            message: 'Unauthorized'
+          })
+        }
       }
     }
   }
