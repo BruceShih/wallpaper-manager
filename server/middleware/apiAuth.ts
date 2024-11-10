adc905ad57356d47b0d8385930eebceff55152fe
Bruce Shih <sp1986@gmail.com>
2024-11-10 23:43:10 +0800

    fix: temporary comment out (revert later if works)

--- server/middleware/apiAuth.ts
+++ server/middleware/apiAuth.ts
@@ -1,22 +1,22 @@
 import process from 'node:process'
 
 export default defineEventHandler((event) => {
-  const paths = ['/api/delete', '/api/list', '/api/get', '/api/update', '/api/upload']
-  const referer = event.headers.get('referer')
+  // const paths = ['/api/delete', '/api/list', '/api/get', '/api/update', '/api/upload']
+  // const referer = event.headers.get('referer')
 
-  if (paths.some(path => event.path.startsWith(path))) {
-    if (referer) {
-      if (new URL(referer).origin !== process.env.ORIGIN) {
-        const { authKeySecret } = useRuntimeConfig(event)
-        const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
-        if (key !== authKeySecret) {
-          console.error('[Wallpaper Service] Unauthorized')
-          throw createError({
-            statusCode: 401,
-            message: 'Unauthorized'
-          })
-        }
-      }
-    }
-  }
+  // if (paths.some(path => event.path.startsWith(path))) {
+  //   if (referer) {
+  //     if (new URL(referer).origin !== process.env.ORIGIN) {
+  //       const { authKeySecret } = useRuntimeConfig(event)
+  //       const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
+  //       if (key !== authKeySecret) {
+  //         console.error('[Wallpaper Service] Unauthorized')
+  //         throw createError({
+  //           statusCode: 401,
+  //           message: 'Unauthorized'
+  //         })
+  //       }
+  //     }
+  //   }
+  // }
 })
