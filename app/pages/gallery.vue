3bd9cd632a7f56c9d598a7581cb64ce94866ee4e
Bruce Shih <sp1986@gmail.com>
2024-11-10 12:07:26 +0800

    fix: use my api path instead of r2 custom domain to get image

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -122,7 +122,7 @@ function onPageChange(page: number) {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <NuxtImg class="w-full h-64" :src="`/images/${image.key}`" />
+          <NuxtImg class="w-full h-64" :src="`/get/${image.key}`" />
         </CardContent>
         <CardFooter class="justify-start">
           <Button variant="ghost" size="icon">
