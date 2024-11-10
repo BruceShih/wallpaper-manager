320550d053c3ca0929f3fa3e623151e5364e5c65
Bruce Shih <sp1986@gmail.com>
2024-11-10 23:18:40 +0800

    fix: 401 for same domain

--- server/middleware/apiAuth.ts
+++ server/middleware/apiAuth.ts
@@ -1,22 +1,16 @@
 import process from 'node:process'
 
 export default defineEventHandler((event) => {
+  const paths = ['/api/delete', '/api/list', '/api/get', '/api/update', '/api/upload']
   const referer = event.headers.get('referer')
-  if (referer) {
-    // check if the request origin is the same as the API Gateway domain
-    if (new URL(referer).origin !== process.env.ORIGIN) {
-      const paths = ['/api/delete', '/api/list', '/api/get', '/api/update', '/api/upload']
-      // if event.path starts with any of paths above
-      if (paths.some(path => event.path.startsWith(path))) {
-      // get authKeySecret from runtime config
+
+  if (paths.some(path => event.path.startsWith(path))) {
+    if (referer) {
+      if (new URL(referer).origin !== process.env.ORIGIN) {
         const { authKeySecret } = useRuntimeConfig(event)
-        // get key from request header
         const key = getRequestHeader(event, 'X-Bucket-Auth-Key')
-        // if key is not equal to authKeySecret
         if (key !== authKeySecret) {
-        // log unauthorized
           console.error('[Wallpaper Service] Unauthorized')
-          // throw error
           throw createError({
             statusCode: 401,
             message: 'Unauthorized'
