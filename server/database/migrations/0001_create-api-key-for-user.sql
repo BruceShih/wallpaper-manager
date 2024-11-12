e9b495a02f9bb71dac2c5b093a5245d634bd4845
Bruce Shih <sp1986@gmail.com>
2024-11-12 13:23:52 +0800

    chore: remove migrated file

--- server/database/migrations/0001_create-api-key-for-user.sql
+++
@@ -1,9 +0,0 @@
--- Migration number: 0001 	 2024-11-12T05:03:27.242Z
-CREATE TABLE `userToken` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`userId` text NOT NULL,
-	`token` text NOT NULL,
-	`enabled` integer DEFAULT 0 NOT NULL,
-	`createDate` text NOT NULL,
-	`deleteDate` text
-);
