8e0e3c3360f8e55bbfb3a5db6818486da1cda262
Bruce Shih <sp1986@gmail.com>
2024-11-12 21:43:17 +0800

    feat: use tokens in userToken to verify requests not coming from cloudflare

--- server/middleware/apiAuth.ts
+++ server/middleware/apiAuth.ts
@@ -1,13 +1,39 @@
+import { userToken } from '../database/schema'
+
+// only authenticate requests not from cloudflare workers
 export default defineEventHandler(async (event) => {
   if (event.path.startsWith('/api/image')) {
-    const session = await serverAuth().api.getSession({
-      headers: event.headers
-    })
+    const authHeader = event.headers.get('Authorization')
 
-    if (!session) {
+    if (!authHeader) {
       throw createError({
         statusCode: 401,
         message: 'Unauthorized'
+      })
+    }
+
+    const token = authHeader.replace('Bearer ', '')
+
+    try {
+      const allTokens = await useDrizzle()
+        .select()
+        .from(userToken)
+        .where(eq(userToken.enabled, true))
+
+      const validToken = allTokens.find(t => t.token === token)
+
+      if (!validToken) {
+        throw createError({
+          statusCode: 401,
+          message: 'Unauthorized'
+        })
+      }
+    }
+    catch (error) {
+      console.error('[Wallpaper Service] Server error:', error)
+      throw createError({
+        statusCode: 500,
+        message: 'Server error'
       })
     }
   }
