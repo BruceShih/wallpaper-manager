c79442f22aaf4ebf162b31ebd724612602724618
Bruce Shih <sp1986@gmail.com>
2024-11-12 13:20:33 +0800

    fix: fix schema error

--- server/database/migrations/0001_create-api-key-for-user.sql
+++ server/database/migrations/0001_create-api-key-for-user.sql
@@ -1,14 +1,9 @@
 -- Migration number: 0001 	 2024-11-12T05:03:27.242Z
-CREATE TABLE userToken (
-	id INTEGER,
-	userId TEXT NOT NULL,
-	token TEXT NOT NULL,
-	enabled INTEGER NOT NULL,
-	createDate TEXT NOT NULL,
-	deleteDate TEXT NOT NULL,
-  PRIMARY KEY (id)
-  FOREIGN KEY (userId) 
-    REFERENCES user (id) 
-      ON DELETE CASCADE 
-      ON UPDATE NO ACTION,
+CREATE TABLE `userToken` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`userId` text NOT NULL,
+	`token` text NOT NULL,
+	`enabled` integer DEFAULT 0 NOT NULL,
+	`createDate` text NOT NULL,
+	`deleteDate` text
 );
