58b32aff24739d9ffc323ef9a344f9e1f3153e79
Bruce Shih <sp1986@gmail.com>
2024-11-12 16:04:49 +0800

    chore: remove migrated sql file

--- server/database/migrations/0001_auto-increment-user-token-table.sql
+++
@@ -1,9 +0,0 @@
--- Migration number: 0001 	 2024-11-12T07:59:22.036Z
-CREATE TABLE [userToken] (
-  "id" integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-  "userId" text NOT NULL,
-  "token" text NOT NULL,
-  "enabled" integer DEFAULT 0 NOT NULL,
-  "createDate" text NOT NULL,
-  "deleteDate" text
-);
