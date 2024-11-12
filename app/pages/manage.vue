e66fc460321dd8fbcc5d93beb3082033dea02a24
Bruce Shih <sp1986@gmail.com>
2024-11-12 19:10:21 +0800

    fix: type error

--- app/pages/manage.vue
+++ app/pages/manage.vue
@@ -1,56 +1,46 @@
 <script setup lang="ts">
+import type {
+  ColumnDef
+} from '@tanstack/vue-table'
 import type { UserToken } from '~~/server/utils/drizzle'
 import { Icon } from '#components'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
-import {
-  type ColumnFiltersState,
-  createColumnHelper,
-  type ExpandedState,
-  FlexRender,
-  getCoreRowModel,
-  getExpandedRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  type SortingState,
-  useVueTable,
-  type VisibilityState
-} from '@tanstack/vue-table'
 import { useToast } from '~/components/ui/toast/use-toast'
-import { cn, valueUpdater } from '~/lib/utils'
 
 const { toast } = useToast()
 
 const token = ref(import.meta.client ? localStorage.getItem('bearer_token') || '' : '')
-const userToken = reactive<{
-  tokens: UserToken[]
-}>({ tokens: [] })
-const columnHelper = createColumnHelper<UserToken>()
+const userTokens = ref<UserToken[]>([])
 
-const columns = [
-  columnHelper.accessor('token', {
-    id: 'token',
-    header: 'Token',
-    cell: ({ row }) => h('div', {}, row.getValue('token'))
-  }),
-  columnHelper.accessor('createDate', {
-    id: 'createDate',
-    header: 'Create Date',
-    cell: ({ row }) => h('div', {}, row.getValue('createDate'))
-  }),
-  columnHelper.accessor('enabled', {
-    id: 'enabled',
-    header: 'Enabled',
+const columns: ColumnDef<UserToken>[] = [
+  {
+    accessorKey: 'token',
+    header: () => h('div', { class: 'text-right' }, 'Token'),
+    cell: ({ row }) => {
+      return h('div', {}, row.getValue('token'))
+    }
+  },
+  {
+    accessorKey: 'createDate',
+    header: () => h('div', { class: 'text-right' }, 'Create Date'),
+    cell: ({ row }) => {
+      return h('div', {}, row.getValue('createDate'))
+    }
+  },
+  {
+    accessorKey: 'enabled',
+    header: () => h('div', { class: 'text-right' }, 'Create Date'),
     cell: ({ row }) => {
       return h(Checkbox, {
         'checked': row.getValue<boolean>('enabled'),
         'onUpdate:checked': value => updateToken(row.getValue('id'), value)
       })
     }
-  }),
-  columnHelper.display({
+  },
+  {
     id: 'actions',
+    header: () => h('div', { class: 'text-right' }, ''),
     cell: ({ row }) => {
       return h(Button, {
         variant: 'destructive',
@@ -60,34 +50,8 @@ const columns = [
         class: 'size-4'
       }))
     }
-  })
-]
-const sorting = ref<SortingState>([])
-const columnFilters = ref<ColumnFiltersState>([])
-const columnVisibility = ref<VisibilityState>({})
-const rowSelection = ref({})
-const expanded = ref<ExpandedState>({})
-const table = useVueTable({
-  data: userToken.tokens,
-  columns,
-  getCoreRowModel: getCoreRowModel(),
-  getPaginationRowModel: getPaginationRowModel(),
-  getSortedRowModel: getSortedRowModel(),
-  getFilteredRowModel: getFilteredRowModel(),
-  getExpandedRowModel: getExpandedRowModel(),
-  onSortingChange: updaterOrValue => valueUpdater(updaterOrValue, sorting),
-  onColumnFiltersChange: updaterOrValue => valueUpdater(updaterOrValue, columnFilters),
-  onColumnVisibilityChange: updaterOrValue => valueUpdater(updaterOrValue, columnVisibility),
-  onRowSelectionChange: updaterOrValue => valueUpdater(updaterOrValue, rowSelection),
-  onExpandedChange: updaterOrValue => valueUpdater(updaterOrValue, expanded),
-  state: {
-    get sorting() { return sorting.value },
-    get columnFilters() { return columnFilters.value },
-    get columnVisibility() { return columnVisibility.value },
-    get rowSelection() { return rowSelection.value },
-    get expanded() { return expanded.value }
   }
-})
+]
 
 async function fetchTokens() {
   const { data } = await useFetch('/api/token/list', {
@@ -96,7 +60,7 @@ async function fetchTokens() {
     }
   })
 
-  userToken.tokens = data.value || []
+  userTokens.value = data.value || []
 }
 async function createToken() {
   const { error } = await useFetch('/api/token', {
@@ -173,61 +137,9 @@ onMounted(async () => {
         Create new token
       </Button>
     </div>
-    <Table>
-      <TableHeader>
-        <TableRow
-          v-for="headerGroup in table.getHeaderGroups()"
-          :key="headerGroup.id"
-        >
-          <TableHead
-            v-for="header in headerGroup.headers"
-            :key="header.id"
-            :class="cn(
-              { 'w-2/3': header.id === 'token' },
-            )"
-          >
-            <FlexRender
-              v-if="!header.isPlaceholder"
-              :render="header.column.columnDef.header"
-              :props="header.getContext()"
-            />
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <template v-if="table.getRowModel().rows?.length">
-          <template
-            v-for="row in table.getRowModel().rows"
-            :key="row.id"
-          >
-            <TableRow :data-state="row.getIsSelected() && 'selected'">
-              <TableCell
-                v-for="cell in row.getVisibleCells()"
-                :key="cell.id"
-              >
-                <FlexRender
-                  :render="cell.column.columnDef.cell"
-                  :props="cell.getContext()"
-                />
-              </TableCell>
-            </TableRow>
-            <TableRow v-if="row.getIsExpanded()">
-              <TableCell :colspan="row.getAllCells().length">
-                {{ row.original }}
-              </TableCell>
-            </TableRow>
-          </template>
-        </template>
-
-        <TableRow v-else>
-          <TableCell
-            :colspan="columns.length"
-            class="h-24 text-center"
-          >
-            No results.
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <DataTable
+      :columns="columns"
+      :data="userTokens"
+    />
   </div>
 </template>
