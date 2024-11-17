6196860ef61a41ac500e5d0b8ab89dd6a5146a55
Bruce Shih <sp1986@gmail.com>
2024-11-17 23:00:43 +0800

    fix: fix sql

--- server/database/migrations/0002_worthless_fabian_cortez.sql
+++ server/database/migrations/0002_worthless_fabian_cortez.sql
@@ -10,7 +10,7 @@ CREATE TABLE `__new_images` (
 --> statement-breakpoint
 INSERT INTO `__new_images`("key", "tags", "favorite", "alive", "createDate", "deleteDate") SELECT "key", "tags", "favorite", "alive", "createDate", "deleteDate" FROM `images`;--> statement-breakpoint
 DROP TABLE `images`;--> statement-breakpoint
-UPDATE `__new_images` SET `tags` = '[1]' WHERE nsfw = 1;--> statement-breakpoint
-UPDATE `__new_images` SET `tags` = '[]' WHERE nsfw = 0;--> statement-breakpoint
+UPDATE `__new_images` SET `tags` = '[1]' WHERE `nsfw` = 1;--> statement-breakpoint
+UPDATE `__new_images` SET `tags` = '[]' WHERE `nsfw` = 0;--> statement-breakpoint
 ALTER TABLE `__new_images` RENAME TO `images`;--> statement-breakpoint
 PRAGMA foreign_keys=ON;
