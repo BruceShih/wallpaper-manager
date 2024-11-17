aac5438bc3d264f9b319b29e970b9ea0109e0745
Bruce Shih <sp1986@gmail.com>
2024-11-17 22:47:42 +0800

    feat: add tags table and update relation

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -2,6 +2,7 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
 export const images = sqliteTable('images', {
   key: text('key').primaryKey(),
+  tags: text('tags', { mode: 'json' }).$type<number[]>().notNull(),
   nsfw: integer({ mode: 'boolean' }).notNull().default(false),
   favorite: integer({ mode: 'boolean' }).notNull().default(false),
   alive: integer({ mode: 'boolean' }).notNull().default(true),
@@ -16,4 +17,10 @@ export const userToken = sqliteTable('userToken', {
   enabled: integer({ mode: 'boolean' }).notNull().default(false),
   createDate: text('createDate').notNull(),
   deleteDate: text('deleteDate')
+})
+
+export const tags = sqliteTable('tags', {
+  id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
+  tag: text('tag').notNull(),
+  enabled: integer({ mode: 'boolean' }).notNull().default(true)
 })
