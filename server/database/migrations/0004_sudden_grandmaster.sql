ba3073f06f9bbdc393df4ac2da1480490b0ec439
Bruce Shih <sp1986@gmail.com>
2024-11-18 01:24:15 +0800

    fix: im so bad with sql

--- server/database/migrations/0004_sudden_grandmaster.sql
+++ server/database/migrations/0004_sudden_grandmaster.sql
@@ -5,6 +5,6 @@ CREATE TABLE `imagesToTags` (
 	FOREIGN KEY (`imageKey`) REFERENCES `images`(`key`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`tagId`) REFERENCES `tags`(`id`) ON UPDATE no action ON DELETE no action
 );
-INSERT INTO `imagesToTags` (`imageKey`, `tagId`) SELECT `key`, `1` FROM `images` WHERE `tags` = '[1]';--> statement-breakpoint
+INSERT INTO `imagesToTags` (`imageKey`, `tagId`) SELECT `key`, 1 FROM `images` WHERE `tags` = '[1]';--> statement-breakpoint
 --> statement-breakpoint
 ALTER TABLE `images` DROP COLUMN `tags`;
