eb76b17cdac2c30914b150c1ff587645890611bf
Bruce Shih <sp1986@gmail.com>
2024-11-19 01:09:43 +0800

    fix: currently there is no safe tag so

--- server/api/image/get/index.get.ts
+++ server/api/image/get/index.get.ts
@@ -1,7 +1,6 @@
 import { images, imagesToTags, tags } from '~~/server/database/schema'
-import { and, eq, sql, useDrizzle } from '~~/server/utils/drizzle'
+import { and, eq, type ImagesToTags, sql, useDrizzle } from '~~/server/utils/drizzle'
 import { apiImageGetQuerySchema } from '~~/server/utils/validator'
-import { inArray } from 'drizzle-orm'
 
 export default eventHandler(async (event) => {
   const query = await getValidatedQuery(event, data => apiImageGetQuerySchema.safeParse(data))
@@ -12,27 +11,42 @@ export default eventHandler(async (event) => {
     })
   }
 
+  let imageQuery: Image[] | { imagesToTags: ImagesToTags, tags: Tag | null, images: Image | null }[]
+  let id: string = ''
+  let favorite: boolean = false
+
   try {
-    const sensitive = query.data.sensitive ? query.data.sensitive : false
-    const imageQuery = await useDrizzle()
-      .select()
-      .from(images)
-      .leftJoin(imagesToTags, eq(images.key, imagesToTags.imageKey))
-      .leftJoin(tags, eq(imagesToTags.tagId, tags.id))
-      .where(and(
-        eq(images.alive, true),
-        eq(tags.enabled, true),
-        eq(tags.sensitive, sensitive)
-      ))
-      .orderBy(sql`RANDOM()`)
-      .limit(1)
-    // const imageQuery = await useDrizzle().query.images.findFirst({
-    //   where: and(
-    //     eq(images.alive, true),
-    //     inArray(images.key, imagesToTagsQuery.map(row => row.imageKey))
-    //   ),
-    //   orderBy: sql`RANDOM()`
-    // })
+    const sensitive = query.data.sensitive
+    if (sensitive) {
+      imageQuery = await useDrizzle()
+        .select()
+        .from(imagesToTags)
+        .leftJoin(tags, eq(imagesToTags.tagId, tags.id))
+        .leftJoin(images, eq(imagesToTags.imageKey, images.key))
+        .where(and(
+          eq(images.alive, true),
+          eq(tags.enabled, true),
+          eq(tags.sensitive, sensitive)
+        ))
+        .orderBy(sql`RANDOM()`)
+        .limit(1)
+
+      id = imageQuery[0]?.images?.key || ''
+      favorite = imageQuery[0]?.images?.favorite || false
+    }
+    else {
+      imageQuery = await useDrizzle()
+        .select()
+        .from(images)
+        .where(and(
+          eq(images.alive, true)
+        ))
+        .orderBy(sql`RANDOM()`)
+        .limit(1)
+
+      id = imageQuery[0]?.key || ''
+      favorite = imageQuery[0]?.favorite || false
+    }
 
     const randomRow = imageQuery[0]
 
@@ -41,9 +55,6 @@ export default eventHandler(async (event) => {
         statusCode: 404
       })
     }
-
-    const id = randomRow.images.key
-    const favorite = randomRow.images.favorite
 
     setResponseHeaders(event, {
       'Image-Id': id,
