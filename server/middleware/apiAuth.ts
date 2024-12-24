73ea592edcf4d0e156bb90d98e6e9ed6e17b8a62
Bruce Shih <sp1986@gmail.com>
2024-12-24 21:27:16 +0800

    fix: only guard routes under /api

--- server/middleware/apiAuth.ts
+++ server/middleware/apiAuth.ts
@@ -22,32 +22,34 @@ export default defineEventHandler(async (event) => {
     = headers.origin.startsWith(origin) || headers.referer.startsWith(origin)
 
   if (!isFromCloudflare) {
-    const authHeader = event.headers.get('Authorization')
+    if (event.path.startsWith('/api/')) {
+      const authHeader = event.headers.get('Authorization')
 
-    if (!authHeader) {
-      consola.error(new Error('Missing authorization header'))
-      throw createError({ statusCode: 403 })
-    }
-
-    const token = authHeader.replace('Bearer ', '')
-
-    try {
-      const allTokens = await useDrizzle()
-        .select()
-        .from(userToken)
-        .where(eq(userToken.enabled, true))
-
-      const validToken = allTokens.find(t => t.token === token)
-
-      if (!validToken) {
-        consola.error(new Error('No valid token found'))
+      if (!authHeader) {
+        consola.error(new Error('Missing authorization header'))
         throw createError({ statusCode: 403 })
       }
-    }
-    catch (error) {
-      if (error instanceof Error) {
-        consola.error(new Error('Server error'))
-        throw createError(error)
+
+      const token = authHeader.replace('Bearer ', '')
+
+      try {
+        const allTokens = await useDrizzle()
+          .select()
+          .from(userToken)
+          .where(eq(userToken.enabled, true))
+
+        const validToken = allTokens.find(t => t.token === token)
+
+        if (!validToken) {
+          consola.error(new Error('No valid token found'))
+          throw createError({ statusCode: 403 })
+        }
+      }
+      catch (error) {
+        if (error instanceof Error) {
+          consola.error(new Error('Server error'))
+          throw createError(error)
+        }
       }
     }
   }
